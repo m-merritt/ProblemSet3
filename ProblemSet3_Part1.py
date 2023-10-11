1cb553b7a67f9d265211057f6f53460884cfcd21
m-merritt <melissa.merritt@duke.edu>
2023-10-11 12:20:29 -0400

    task 1

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,11 @@
 #%% Task 1 - Edit code to print as requested
-/*-PS3: Code Block 1--*/
+#/*-PS3: Code Block 1--*/
 
 mountain = "Denali"
 nickname = 'Mt. McKinley'
-elevation == 20322 
+elevation = 20322 
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+print (str(mountain) + ', formerly \nknown as "' + str(nickname) + '",')
+print ("is {}' above sea level.".format(elevation))
 
 #%% Task 2 - Lists and Iteration
