19f39044d7fea9dbbbfd46872b1e0c68ecc8006c
m-merritt <melissa.merritt@duke.edu>
2023-10-11 13:20:34 -0400

    tasks 2 and 3 in part 1

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -9,3 +9,28 @@ print (str(mountain) + ', formerly \nknown as "' + str(nickname) + '",')
 print ("is {}' above sea level.".format(elevation))
 
 #%% Task 2 - Lists and Iteration
+
+data_format = r'W:\859_data\triangle'
+data_list = ['streams.shp', 'stream_types.csv', 'naip_imagery.tif']
+user_item = 'roads.shp'
+
+data_list.append(user_item)
+
+for data in data_list:
+    print(data_format + "\\" + data)
+
+#%% Task 3 - Lists and Iteration
+
+user_numbers = []
+
+for x in range(3):
+    guess = input("Enter an integer: ")
+    user_numbers.append(int(guess))
+user_numbers.sort()
+print(user_numbers[2])
+
+
+
+    
+
+# %%
